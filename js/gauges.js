cc2b074bd21728f146d3d95aeb0b4e723838b6e8
DKevinM <35807537+DKevinM@users.noreply.github.com>
2026-01-11 15:26:10 -0600

    Update gauges.js

--- js/gauges.js
+++ js/gauges.js
@@ -1,3 +1,17 @@
 function buildGauges(data) {
   console.log("Build gauges for:", data);
 }
+
+function buildGauges(station) {
+  const container = document.getElementById("station-gauges");
+
+  const data = [{
+    type: "indicator",
+    mode: "gauge+number",
+    value: station.AQHI ?? 0,
+    title: { text: "AQHI" },
+    gauge: { axis: { range: [0, 11] } }
+  }];
+
+  Plotly.newPlot(container, data, { height: 300 });
+}
