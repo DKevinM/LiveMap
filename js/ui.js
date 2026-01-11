926c28a33d635fd6c1f5ac3e9a99ff80bbe35429
DKevinM <35807537+DKevinM@users.noreply.github.com>
2026-01-11 15:21:13 -0600

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,18 @@
 function showStationModal(station) {
   alert("Station: " + station.name);
 }
+
+function getAQHIColor(val) {
+  if (!val || val < 1) return "#D3D3D3";
+  if (val == 1) return "#01cbff";
+  if (val == 2) return "#0099cb";
+  if (val == 3) return "#016797";
+  if (val == 4) return "#fffe03";
+  if (val == 5) return "#ffcb00";
+  if (val == 6) return "#ff9835";
+  if (val == 7) return "#fd6866";
+  if (val == 8) return "#fe0002";
+  if (val == 9) return "#cc0001";
+  if (val == 10) return "#9a0100";
+  return "#640100";
+}
