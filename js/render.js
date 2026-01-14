8cb7981e7fb40c89ca503386649a29d6da9efa43
DKevinM <35807537+DKevinM@users.noreply.github.com>
2026-01-14 09:53:06 -0600

    Update render.js

--- js/render.js
+++ js/render.js
@@ -1,5 +1,31 @@
 window.renderMap = function () {
 
+function normalizeStationForGauges(st) {
+
+  const params = {};
+  const list = window.dataByStation?.[st.stationName] || [];
+
+  list.forEach(r => {
+    const p = r.ParameterName;
+    const v = Number(r.Value);
+
+    if (p === "AQHI") params.AQHI = v;
+    if (p === "Fine Particulate Matter") params.PM25 = v;
+    if (p === "Ozone") params.O3 = v;
+    if (p === "Nitrogen Dioxide") params.NO2 = v;
+    if (p === "Relative Humidity") params.RH = v;
+    if (p === "Outdoor Temperature") params.Temp = v;
+    if (p === "Wind Speed") params.WS = v;
+  });
+
+  return {
+    ...st,
+    ...params
+  };
+}
+
+
+  
   const map = window.map;
 
   const stationLayer = L.layerGroup().addTo(map);
@@ -51,3 +77,18 @@ window.renderMap = function () {
 
   console.log("Map rendered.");
 };
+
+
+marker.on("click", () => {
+
+  const fullStation = normalizeStationForGauges(st);
+
+  if (typeof showStationModal === "function") {
+    showStationModal(fullStation);
+  }
+
+  if (typeof showGaugesForStation === "function") {
+    showGaugesForStation(fullStation);
+  }
+
+});
