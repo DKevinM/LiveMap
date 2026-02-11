982a60258eb21368fe319dca36fa01a877c1005d
DKevinM <35807537+DKevinM@users.noreply.github.com>
2026-02-11 08:55:19 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
 window.bootstrap = async function () {
 
-  console.log("Bootstrapping clean app...");
 
   await initMap();
-  await AppData.ready;
-  await window.dataReady;
 
-  await window.buildDataStructures();
+  await window.dataReady;
   
   await window.renderMap();   // ← the real renderer
 
-  console.log("Application ready.");
 };
 
 window.addEventListener("load", bootstrap);
