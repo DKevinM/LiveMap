930ace915cb5d271e06360dd19429bfb352acf62
DKevinM <35807537+DKevinM@users.noreply.github.com>
2026-04-14 16:02:20 -0600

    Update map.js

--- js/map.js
+++ js/map.js
@@ -45,14 +45,16 @@ window.initMap = function () {
 
   window.map = map;
 
-  L.control({position: "bottomright"})
-    .onAdd = () => {
-      const img = L.DomUtil.create("img");
-      img.src = "images/aqhi_legend.png";
-      img.style.width = "180px";
-      return img;
-    }
-    .addTo(map);  
+  const legend = L.control({ position: "bottomright" });
+  
+  legend.onAdd = function () {
+    const img = L.DomUtil.create("img");
+    img.src = "images/aqhi_legend.png";
+    img.style.width = "180px";
+    return img;
+  };
+  
+  legend.addTo(map);
 
   // ----------------------------
   // AQHI CLICK HANDLER
